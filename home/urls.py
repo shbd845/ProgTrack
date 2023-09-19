9a7a6bbd85f449e74cee636651d41e790ac1d659
shbd845 <shubhagrawal74824@gmail.com>
2023-09-19 13:55:40 +0530

    Tasks Page Created

--- home/urls.py
+++ home/urls.py
@@ -21,5 +21,5 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('/about', views.about, name='about')
+    path('tasks', views.tasks, name='tasks')
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request,"index.html")
+def tasks(request):
+    return render(request,"tasks.html")
