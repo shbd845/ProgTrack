27890295fd786cddff531ba4756960c3dab7f3e4
shbd845 <shubhagrawal74824@gmail.com>
2023-09-19 14:40:44 +0530

    Model Created

--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render, HttpResponse
-
+from home.models import Task
 # Create your views here.
 def home(request):
-    return render(request,"index.html")
+    context={'success':False}
+    if request.method=="POST":
+        title=request.POST["title"]
+        desc=request.POST["desc"]
+        print(title,desc)
+        ins=Task(taskTitle=title,taskDesc=desc)
+        ins.save()
+        context={'success':True}
+
+    return render(request,"index.html",context)
 def tasks(request):
     return render(request,"tasks.html")
